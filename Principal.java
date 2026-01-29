1b0d3f7eee4fc74b5227a3d9a556d5e7b625fea0
FenorDev <david.fenor96@gmail.com>
2026-01-29 13:20:49 +0100

    Clase Usuario corregida

--- Principal.java
+++ Principal.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Principal {
-	public static void main (String[] args) {
-		
+	public static Scanner teclado = new Scanner(System.in);
+	public static Usuario[] crearUsuarios(int n) {
+	Usuario[] usuarios = new Usuario[n];
 		System.out.println("Hola Mundo");
+
+		for (int i = 0; i < n; i++) {
+			usuarios[i] = new Usuario();
+
+			System.out.println("Nombre: ");
+			usuarios[i].nombre = teclado.nextLine();
+				
+			System.out.println("Apellidos: ");
+			usuarios[i].apellidos = teclado.nextLine();
+
+			System.out.println("Email: ");
+			usuarios[i].email = teclado.nextLine();
+			
+		}
+		return usuarios;
+	}
+	public static void main(String[] args) {
+		Usuario[] lista = crearUsuarios(2);
+
+		for (Usuario u : lista) {
+			System.out.println(u.nombre + " " + u.apellidos + " - " + u.email);
+		}
 	}
 }
